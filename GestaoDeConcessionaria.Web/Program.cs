d2352421fc876e8772983926021dd20ee5e00f6a
thalesaugustodias <thalesaugustodias98@gmail.com>
2025-02-22 00:08:11 -0300

    Migração de Razor Pages para MVC
    As mudanças realizadas no código indicam uma migração de um projeto ASP.NET Core Razor Pages para um projeto ASP.NET Core MVC. A estrutura do projeto foi ajustada para usar controladores e ações em vez de páginas Razor.
    
    Principais mudanças:
    - `Error.cshtml` atualizado para usar `ErrorViewModel`.
    - Remoção de código em `Error.cshtml.cs`, `Index.cshtml.cs` e `Privacy.cshtml.cs`.
    - Atualização de `Index.cshtml` e `Privacy.cshtml` para usar `ViewData`.
    - Atualização de `_Layout.cshtml` para usar controladores e ações.
    - Reformatação de `_Layout.cshtml.css` e `_ValidationScriptsPartial.cshtml`.
    - Adição de diretivas em `_ViewImports.cshtml`.
    - Configuração de serviços e roteamento em `Program.cs`.
    - Atualização de URLs em `launchSettings.json`.
    - Adição de `HomeController` e `ErrorViewModel`.
    - Atualização do arquivo de solução `GestaoDeConcessionaria.sln`.

--- GestaoDeConcessionaria.Web/Program.cs
+++ GestaoDeConcessionaria.Web/Program.cs
@@ -1,12 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,6 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
